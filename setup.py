ec68ad05d475326579da0921db2e4d65a5c896ed
Omarjan <125962569+wubulks@users.noreply.github.com>
2026-02-11 22:54:13 +0800

    Bump version from 0.1.0 to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="figengine",
-    version="0.1.0",
+    version="1.1.0",
     description="High-Performance Structured Figure Engine",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
